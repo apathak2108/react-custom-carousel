992757630b2d466f0d815ec8d97dd6ad5f1f976f
Anany Pathak <propanany@gmail.com>
2024-07-02 11:09:44 +0530

    ui

---
+++ src/pages/home/home.styled.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const StyledSlider = styled.div`
+  width: 100%;
+  position: relative;
+`;
+
+export const StyledImage = styled.img`
+  height: 580px;
+  width: 100%;
+  margin-top: 12px;
+//   display: none;
+`;
+
+export const StyledIcon = styled.img`
+  border: none;
+  background-color: transparent;
+  font-size: 70px;
+  cursor: pointer;
+  position: absolute;
+  top: 50%;
+`;
